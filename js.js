463bc1a3ec178d356e26e3f0f2563c125b937c6c
student <Muthoniyahya@gmail.com>
2021-03-29 09:16:37 +0300

    first commit

---
+++ js.js
@@ -0,0 +1,15 @@
+var kiosk={
+    fruit:'Grapes',
+    setFruit(){
+        return this.fruit;
+    },
+    getFruits(fruits){
+        return this.fruits=fruit;
+    }
+
+}
+
+console.log(kiosk.fruit);
+kiosk.fruits=['Watermelon','Avocado'];
+console.log(kiosk.fruits);
+    
